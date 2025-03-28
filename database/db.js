4236da2e97e15fb642d218f196023d9270276088
SuReSh <sureshc.btech2022@iujaipur.edu.in>
2025-03-28 09:37:01 +0530

    commit2

--- database/db.js
+++ database/db.js
@@ -1,12 +1,15 @@
 import { connect } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
 
 const connectToMongo = async () => {
   try {
-    await connect('mongodb+srv://krishan7877:7877@mongodb.snwfo.mongodb.net/enotebook');
-    console.log("---***Database Connected Successfully***---")
+    await connect(process.env.MONGO_URI);
+    console.log("---***Database Connected Successfully***---");
   } catch (error) {
-    console.log(error);
+    console.log("Database Connection Error:", error);
   }
-}
+};
 
 export default connectToMongo;
